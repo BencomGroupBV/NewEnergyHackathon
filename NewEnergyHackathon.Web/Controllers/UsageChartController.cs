e246c96a3435b43ae1e74099b7298017ba81c867
Frens Buning <frens@bencom.nl>
2025-05-17 11:29:55 +0200

    Made the datepicker variable!

--- NewEnergyHackathon.Web/Controllers/UsageChartController.cs
+++ NewEnergyHackathon.Web/Controllers/UsageChartController.cs
@@ -11,109 +11,102 @@ namespace NewEnergyHackathon.Web.Controllers;
 [ApiController]
 public class UsageChartController(INedService nedService, IBenCompareService benCompareService, IMemoryCache cache) : Controller
 {
-  private const string DateOfEnergyConsumption = "2025-03-22";
+  //private const string DateOfEnergyConsumption = "2025-03-22";
 
   [HttpGet("daily-green-energy")]
   public async Task<IActionResult> GetResults([FromQuery] DateOnly dateTo, DateOnly dateFrom)
   {
+	var date = dateFrom.ToString("yyyy-MM-dd");
 
-    if (!cache.TryGetValue("solar", out var solar))
-    {
-      solar = await nedService.GetGridConsumptionByEnergyType(EnergyType.Solar, dateTo, dateFrom);
-      cache.Set("solar", solar);
-    }
+	if (dateTo == dateFrom)
+	{
+	  dateTo = dateFrom.AddDays(2);
+	}
 
-    if (!cache.TryGetValue("wind", out var wind))
-    {
-      wind = await nedService.GetGridConsumptionByEnergyType(EnergyType.Wind, dateTo, dateFrom);
-      cache.Set("wind", wind);
-    }
+	var solar = await nedService.GetGridConsumptionByEnergyType(EnergyType.Solar, dateTo, dateFrom);
+	var wind = await nedService.GetGridConsumptionByEnergyType(EnergyType.Wind, dateTo, dateFrom);
+	var totalmix = await nedService.GetGridConsumptionByEnergyType(EnergyType.ElectricityMix, dateTo, dateFrom);
 
-    if (!cache.TryGetValue("totalmix", out var totalmix))
-    {
-      totalmix = await nedService.GetGridConsumptionByEnergyType(EnergyType.ElectricityMix, dateTo, dateFrom);
-      cache.Set("totalmix", totalmix);
-    }
 
-    using (Py.GIL())
-    {
-      dynamic sys = Py.Import("sys");
-      sys.path.append(".");
+	using (Py.GIL())
+	{
+	  dynamic sys = Py.Import("sys");
+	  sys.path.append(".");
 
-      dynamic pythonCalculationFile = Py.Import("datawrangling");
+	  dynamic pythonCalculationFile = Py.Import("datawrangling");
 
-      var dailyGreenEnergyGrid = pythonCalculationFile
-        .percentageNEDGreenEnergySingleDay(solar, wind, totalmix, DateOfEnergyConsumption);
+	  var dailyGreenEnergyGrid = pythonCalculationFile
+		.percentageNEDGreenEnergySingleDay(solar, wind, totalmix, date);
 
-      var rawjson = dailyGreenEnergyGrid[0].ToString();
-      var gridGreenScore = (double)dailyGreenEnergyGrid[1];
+	  var rawjson = dailyGreenEnergyGrid[0].ToString();
+	  var gridGreenScore = (double)dailyGreenEnergyGrid[1];
 
-      var gridData = JsonSerializer.Deserialize<List<EnergyMixEntry>>(rawjson);
+	  var gridData = JsonSerializer.Deserialize<List<EnergyMixEntry>>(rawjson);
 
-      var energyMixModel = new EnergyMixDataWrapper
-      {
-        Data = gridData,
-        DailyGreenScoreGrid = gridGreenScore
-      };
+	  var energyMixModel = new EnergyMixDataWrapper
+	  {
+		Data = gridData,
+		DailyGreenScoreGrid = gridGreenScore
+	  };
 
-  
 
-      return Ok(energyMixModel);
-    }
+
+	  return Ok(energyMixModel);
+	}
   }
 
   [HttpGet("daily-green-consumption")]
   public async Task<IActionResult> GetGridConsumption([FromQuery] DateOnly dateTo, DateOnly dateFrom)
   {
-    if (!cache.TryGetValue("solar", out var solar))
-    {
-      solar = await nedService.GetGridConsumptionByEnergyType(EnergyType.Solar, dateTo, dateFrom);
-      cache.Set("solar", solar);
-    }
+	if (!cache.TryGetValue("solar", out var solar))
+	{
+	  solar = await nedService.GetGridConsumptionByEnergyType(EnergyType.Solar, dateTo, dateFrom);
+	  cache.Set("solar", solar);
+	}
 
-    if (!cache.TryGetValue("wind", out var wind))
-    {
-      wind = await nedService.GetGridConsumptionByEnergyType(EnergyType.Wind, dateTo, dateFrom);
-      cache.Set("wind", wind);
-    }
+	if (!cache.TryGetValue("wind", out var wind))
+	{
+	  wind = await nedService.GetGridConsumptionByEnergyType(EnergyType.Wind, dateTo, dateFrom);
+	  cache.Set("wind", wind);
+	}
 
-    if (!cache.TryGetValue("totalmix", out var totalmix))
-    {
-      totalmix = await nedService.GetGridConsumptionByEnergyType(EnergyType.ElectricityMix, dateTo, dateFrom);
-      cache.Set("totalmix", totalmix);
-    }
+	if (!cache.TryGetValue("totalmix", out var totalmix))
+	{
+	  totalmix = await nedService.GetGridConsumptionByEnergyType(EnergyType.ElectricityMix, dateTo, dateFrom);
+	  cache.Set("totalmix", totalmix);
+	}
 
-    using (Py.GIL())
-    {
-      dynamic sys = Py.Import("sys");
-      sys.path.append(".");
+	using (Py.GIL())
+	{
+	  dynamic sys = Py.Import("sys");
+	  sys.path.append(".");
 
-      dynamic pythonCalculationFile = Py.Import("datawrangling");
+	  dynamic pythonCalculationFile = Py.Import("datawrangling");
 
-      var bencompareData = benCompareService.GetBencompareData(DateOfEnergyConsumption);
+	  var bencompareData = benCompareService.GetBencompareData(dateFrom.ToString("yyyy-MM-dd"));
 
-      var result = pythonCalculationFile.greenBehaviourPercentagesSingleDaySingleNonSolarUser(
-        bencompareData,
-        solar,
-        wind,
-        totalmix,
-        DateOfEnergyConsumption
-      );
+	  var result = pythonCalculationFile.greenBehaviourPercentagesSingleDaySingleNonSolarUser(
+		bencompareData,
+	  solar,
+		wind,
+		totalmix,
+		dateFrom.ToString("yyyy-MM-dd")
+	  );
 
-      var rawJson = result[0].ToString();
-      var userGreenScore = (double)result[1];
-      var gridGreenScore = (double)result[2];
+	  var rawJson = result[0].ToString();
+	  var userGreenScore = (double)result[1];
+	  var gridGreenScore = (double)result[2];
 
-      var userScoreList = JsonSerializer.Deserialize<List<UserScoreData>>(rawJson);
-      
-      var bencompareModel = new UserScoreWrapper
-      {
-        Data = userScoreList,
-        PercentageOfUserDailyGreenConsumption = userGreenScore,
-        PercentageOfGridDailyGreenConsumption = gridGreenScore
-      };
+	  var userScoreList = JsonSerializer.Deserialize<List<UserScoreData>>(rawJson);
 
-      return Ok(bencompareModel);
-    }
+	  var bencompareModel = new UserScoreWrapper
+	  {
+		Data = userScoreList,
+		PercentageOfUserDailyGreenConsumption = userGreenScore,
+		PercentageOfGridDailyGreenConsumption = gridGreenScore
+	  };
+
+	  return Ok(bencompareModel);
+	}
   }
 }
